b957552e2a02fbc2c754a4a3974d2cde1d1fed7c
Momo <mohandguellal.dev@gmail.com>
2024-10-15 15:37:19 +0100

    11

--- location/src/components/vehicules.jsx
+++ location/src/components/vehicules.jsx
@@ -20,16 +20,14 @@ export default function vehicules() {
     {load ? (garages.map(
         garage => ( 
             <div className="h-[360px] w-[250px] rounded-[10px] bg-[#C2CAE7] flex flex-col justify-center items-center  relative">
-             <img src={garage.image} alt=""  className="h-[160px] w-[220px]" />   
-            <div className="flex items-start w-[100%] pl-3 mb-2  "><h1 className="text-[#2948A6] font-bold ">{garage.title}</h1></div>
-            <div className="flex items-start w-[100%] pl-3 mb-2"><h1 className="text-[#3ea629] font-bold ">Disponible</h1></div>
-            <div className="flex justify-end w-[100%] mr-5 mb-2"><h1 className="text-[#272727] font-bold ">{garage.price} DA/Jour</h1></div>
-            <div className="flex justify-end w-[100%]  absolute bottom-1 right-1"><button type="submit" className="w-[120px] h-[30px] bg-[#2948A6] rounded-[10px] text-white ">Réserver</button></div>
-            </div>   
-         
+                <img src={garage.image} alt=""  className="h-[160px] w-[220px]" />   
+                <div className="flex items-start w-[100%] pl-3 mb-2  "><h1 className="text-[#2948A6] font-bold ">{garage.title.slice(0,20)+"..."}</h1></div>
+                <div className="flex items-start w-[100%] pl-3 mb-2"><h1 className="text-[#3ea629] font-bold ">Disponible</h1></div>
+                <div className="flex justify-end w-[100%] mr-5 mb-2"><h1 className="text-[#272727] font-bold ">{garage.price} DA/Jour</h1></div>
+                <div className="flex justify-end w-[100%]  absolute bottom-1 right-1"><button type="submit" className="w-[120px] h-[30px] bg-[#2948A6] rounded-[10px] text-white ">Réserver</button></div>
+              </div>     
     )
-    )) : ( <h1>chargement</h1> )}
-    
+    )) : ( <h1>chargement</h1> )}    
     </div>
   )
 }
